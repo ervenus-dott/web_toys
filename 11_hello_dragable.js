83dd3da2a2b9083efc09a4474a6153398ed15032
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-09-02 22:14:48 -0600

    add the ability to open draggable windows

--- 11_hello_dragable.js
+++ 11_hello_dragable.js
@@ -1,3 +1,32 @@
+var imageMap = {
+    capybara: 'https://blog.nature.org/wp-content/uploads/2020/02/29555193323_15d785590f_k.jpg?w=1024', 
+    cat: 'https://i.guim.co.uk/img/media/327aa3f0c3b8e40ab03b4ae80319064e401c6fbc/377_133_3542_2834/master/3542.jpg?width=465&dpr=1&s=none&crop=none',
+    goat: 'https://assets.farmsanctuary.org/content/uploads/2020/05/27054636/2018_10-23_FSNY_Racecar_goat_DSC_5147_Credit_Farm-Sanctuary-1600x1068.jpg',
+};
+var renderWindowContent = (type) => {
+    var x = Math.random() * window.innerWidth;
+    var y = Math.random() * window.innerHeight;
+    return /* html */ `
+    <div class="dragable-window ${type}" style="top: ${y}px; left: ${x}px;">
+        <div class="title-bar">
+            <span>${type} Window</span>
+            <button class="close">x</button>
+        </div>
+        <div class="window-content" style="width: 200px; height: 100px;">
+            <img src="${imageMap[type]}" alt="${type}">
+        </div>
+    </div>
+    `;
+};
+
+document.body.addEventListener('click', (clickEvent) => {
+    // console.log('what is click event', clickEvent);
+    console.log('what is dataset', clickEvent.target.dataset);
+    var windowType = clickEvent.target.dataset.windowType;
+    if (!windowType) {return;}
+    document.body.innerHTML += renderWindowContent(windowType);
+})
+
 var dragableWindow;
 
 var isMouseDown = false;
