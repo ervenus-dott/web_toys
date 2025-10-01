49f222aa2640edab770125c663ccc3321ac9320d
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-09-30 20:25:25 -0600

    add brush preview canvas layer

--- 12_hello_color_web.js
+++ 12_hello_color_web.js
@@ -1,5 +1,7 @@
 const canvas = document.getElementById("color-web")
 const context = canvas.getContext('2d');
+const canvasPreVis = document.getElementById("pre-vis")
+const contextPreVis = canvasPreVis.getContext('2d');
 const tau = Math.PI * 2;
 
 let width = 0;
@@ -9,6 +11,7 @@ let cy = 0;
 let circleVert = [0, 0];
 let mouseVert = [0, 0];
 let isMouseDown = false;
+let currentColor = '#f925';
 
 const resize = function() {
     const rect = canvas.getBoundingClientRect();
@@ -21,12 +24,14 @@ const resize = function() {
         circleVert[1] = cy;
         canvas.width = width;
         canvas.height = height;
+        canvasPreVis.width = width;
+        canvasPreVis.height = height;
     }
 }
 resize()
 // important resize loop making sure that we have up to date height and width info
 
-var drawCircle = (vert, radius, color) => {
+var drawCircle = (vert, radius, color, context) => {
 context.beginPath();
 context.arc(vert[0], vert[1], radius, 0, tau);
 context.fillStyle = color;
@@ -38,19 +43,21 @@ const renderLoop = function(time) {
     resize();
     context.globalCompositeOperation = 'screen';
     if (isMouseDown) {
-        drawCircle(mouseVert, 10, '#19F5');
+        drawCircle(mouseVert, 10, currentColor, context);
     }
+    contextPreVis.clearRect(0, 0, width, height);
+    drawCircle(mouseVert, 10, currentColor, contextPreVis);
 };
 requestAnimationFrame(renderLoop);
 
-canvas.addEventListener('mousemove', (mouseEvent) => {
+canvasPreVis.addEventListener('mousemove', (mouseEvent) => {
     // console.log('what is mouseEvent', mouseEvent);
     mouseVert[0] = mouseEvent.clientX;
     mouseVert[1] = mouseEvent.clientY;
 });
-canvas.addEventListener('mousedown', (mouseEvent) => {
+canvasPreVis.addEventListener('mousedown', (mouseEvent) => {
     isMouseDown = true;
 });
-canvas.addEventListener('mouseup', (mouseEvent) => {
+canvasPreVis.addEventListener('mouseup', (mouseEvent) => {
     isMouseDown = false;
 });
