af92be52e756ee545caf85d97bb37b650e637698
ervenus-dott <120420594+ervenus-dott@users.noreply.github.com>
2025-11-04 17:57:10 -0700

    make points react to being inside the cursor circle

--- 13_hello_reactive_grid.js
+++ 13_hello_reactive_grid.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("circle-grid-outlines");
 const context = canvas.getContext("2d");
 const tau = Math.PI * 2;
 
-let columns = 10;
-let rows = 20;
+let columns = 2;
+let rows = 4;
 let canvasDimensions = canvas.getBoundingClientRect()
 let circleVert = [0, 30];
 let circleArray = [];
@@ -15,9 +15,8 @@ const startup = () => {
 };
 let mouseX = 1;
 let mouseY = 1;
-let ballX = 1;
-let ballY = 1;
-let ballRadius = 20;
+let ballCoords = [1, 1];
+let ballRadius = 40;
 
 window.onload = startup;
 
@@ -48,26 +47,9 @@ const drawArrayCircles = (array) => {
     }
 };
 const moveBall = () => {
-	//get the distance between the mouse and the ball on both axes
-	//walk only the an eight of the distance to create a smooth fadeout
-	let dx = (mouseX - ballX) * .125;
-	let dy = (mouseY - ballY) * .125;
-	//calculate the distance this would move ...
-	let distance = Math.sqrt(dx * dx + dy * dy);
-	//... and cap it at 5px
-	if (distance > 5) {
-		dx *= 20 / distance;
-		dy *= 20 / distance;
-	}
-
-	//now move
-	ballX += dx;
-	ballY += dy;
-
-
-
+    ballCoords = [mouseX, mouseY];
 	context.beginPath();
-	context.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI);
+	context.arc(mouseX, mouseY, ballRadius, 0, 2 * Math.PI);
 	context.fillStyle = 'hsla(25, 100%, 60%, 0.5)';
 	context.fill();
 	context.lineWidth = 5;
@@ -78,19 +60,18 @@ const moveBall = () => {
 
 let loopActive = true;
 const handleCicleUpdate = (circle) => {
-    circle[0] += (Math.random() - 0.5) * 10;
-    circle[1] += (Math.random() - 0.5) * 10;
+    if (
+        circle[0] + 5 > ballCoords[0] - ballRadius &&
+        circle[0] - 5 < ballCoords[0] + ballRadius &&
+        circle[1] + 5 > ballCoords[1] - ballRadius &&
+        circle[1] - 5 < ballCoords[1] + ballRadius
+    ) {
+        circle[0] += (Math.random() - 0.5) * 10;
+        circle[1] += (Math.random() - 0.5) * 10;
+
+    }
     drawCircle(circle, 10, 'white', context);
 };
-const loop = () => {
-    if (loopActive) {
-    requestAnimationFrame(loop);
-    }
-    clear()
-    moveBall()
-    circleArray.forEach(handleCicleUpdate);
-};
-loop();
 
 const loopSwitch = () => {
     if (loopActive) {
@@ -100,20 +81,34 @@ const loopSwitch = () => {
         loop();
     }
 }
-for (let index = 0; index < columns; index++) {
-    let xCoordinate = canvasDimensions.width / (columns + 1);
-    let currentX = xCoordinate * (index + 1);
-    circleVert[0] = currentX;
-    for (let index = 0; index < rows; index++) {
-    let yCoordinate = canvasDimensions.height / (rows + 1);
-    let currentY = yCoordinate * (index + 1);
-    circleVert[1] = currentY;
-    let tempArray = [circleVert[0], circleVert[1]];
-    circleArray.push(tempArray);
-    tempArray = [0, 0];
-    drawCircle(circleVert, 10, 'white', context);
+const drawAndLogCircles = () => {
+    circleArray = [];
+    for (let index = 0; index < columns; index++) {
+        let xCoordinate = canvasDimensions.width / (columns + 1);
+        let currentX = xCoordinate * (index + 1);
+        circleVert[0] = currentX;
+        for (let index = 0; index < rows; index++) {
+        let yCoordinate = canvasDimensions.height / (rows + 1);
+        let currentY = yCoordinate * (index + 1);
+        circleVert[1] = currentY;
+        let tempArray = [circleVert[0], circleVert[1]];
+        circleArray.push(tempArray);
+        tempArray = [0, 0];
+        drawCircle(circleVert, 10, 'white', context);
+        };
     };
 };
+drawAndLogCircles();
+const loop = () => {
+    if (loopActive) {
+    requestAnimationFrame(loop);
+    }
+    clear()
+    circleArray.forEach(handleCicleUpdate);
+    moveBall()
+};
+loop();
+
 const handleMouseMoveEvent = (event) => {
     // should prevent touch scrolling on mobile devices so we can read touch position while the user drags
     event.preventDefault();
